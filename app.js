4837be0860355b67da082f5044f1f4b441032d2b
blackcrabb <niyati20002000@gmail.com>
2020-06-25 18:57:42 +0530

    going on updating story

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const session = require('express-session')
 const passport = require('passport')
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
 const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db');
 
@@ -19,6 +20,22 @@ require('./config/passport')(passport)
 connectDB();
 
 const app = express();
+
+//Body parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
 
 //Logging
 if(process.env.NODE_ENV === 'development') {
@@ -36,14 +53,28 @@ app.use(
     })
 }))
 
+//Handlebars helper
+const { formatDate, stripTags , truncate ,editIcon, select} = require('./helpers/hbs')
+
 
 // Handlebars
-app.engine('.hbs' , exphbs({ defaultLayout: 'main' , extname: '.hbs'}));
+app.engine('.hbs' , exphbs({ 
+    helpers: { formatDate,
+    stripTags, truncate ,
+editIcon,select },
+    defaultLayout: 'main' ,
+     extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//set global var
+app.use(function (req,res, next){
+    res.locals.user = req.user || null
+    next()
+})
 
 //Static folder
 app.use(express.static(path.join(__dirname , 'public')));
@@ -51,6 +82,7 @@ app.use(express.static(path.join(__dirname , 'public')));
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 4500 ;
 
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,8 @@ router.get('/dashboard', ensureAuth,async(req , res)=>{
             stories
         })
     }catch(err){
-        console.log(err)
+        console.error(err)
+        res.render('error/500')
     }
 })
 
